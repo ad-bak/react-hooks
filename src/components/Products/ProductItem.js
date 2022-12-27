61f23dc3e5637efbfdc5e62706cc8c254ea7096e
Adnan <adbak.dev@gmail.com>
2022-12-27 21:44:16 +0100

    replaced redux with react hook

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Card from "../UI/Card";
+import { ProductsContext } from "../../context/products-context";
 import "./ProductItem.css";
-import { toggleFav } from "../../store/actions/products";
 
 const ProductItem = (props) => {
-  const toggleFavHandler = () => {};
+  const toggleFav = useContext(ProductsContext).toggleFav;
+
+  const toggleFavHandler = () => {
+    toggleFav(props.id);
+  };
 
   return (
     <Card style={{ marginBottom: "1rem" }}>
--- src/context/products-context.js
+++ src/context/products-context.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export const ProductsContext = React.createContext({
   products: [],
+  toggleFav: (id) => {},
 });
 
 export default (props) => {
@@ -31,8 +32,24 @@ export default (props) => {
       isFavorite: false,
     },
   ]);
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
+
   return (
-    <ProductsContext.Provider value={{ products: productsList }}>
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
       {props.children}
     </ProductsContext.Provider>
   );
