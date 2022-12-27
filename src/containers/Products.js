16c10b9b1fdb005f0fd2d48d2bc00f029a35613d
Adnan <adbak.dev@gmail.com>
2022-12-28 00:51:40 +0100

    replaced redux with custom hooks

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -3,12 +3,13 @@ import React, { useContext } from "react";
 import ProductItem from "../components/Products/ProductItem";
 import { ProductsContext } from "../context/products-context";
 import "./Products.css";
+import { useStore } from "../hooks-store/store";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import productReducer from "./store/reducers/products";
-import ProductsProvider from "./context/products-context";
+
+import configureProductStore from "./hooks-store/products-store";
+
+configureProductStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
